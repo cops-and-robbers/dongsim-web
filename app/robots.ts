10946e55af514a2a9746edbcaab6314f4adf4cd6
INSANE-P <chanbin0626@gmail.com>
2026-04-29 17:20:03 +0900

    fix: robots.txt에서 비표준 Host 지시문 제거

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,5 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: [{ userAgent: "*", allow: "/" }],
     sitemap: `${BASE_URL}/sitemap.xml`,
-    host: BASE_URL,
   };
 }
