ce93de32199dce80f883344ef4c47a9dfbe7c290
INSANE-P <chanbin0626@gmail.com>
2026-05-04 10:14:26 +0900

    feat: robots.txt에 네이버 Yeti·구글봇 명시적 룰 추가

--- app/robots.ts
+++ app/robots.ts
@@ -4,7 +4,11 @@ const BASE_URL = "https://copsnro66ers.site";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [{ userAgent: "*", allow: "/" }],
+    rules: [
+      { userAgent: "*", allow: "/" },
+      { userAgent: "Yeti", allow: "/" },
+      { userAgent: "Googlebot", allow: "/" },
+    ],
     sitemap: `${BASE_URL}/sitemap.xml`,
   };
 }
